4b2de2a9c4f48d49bb6acf163e5be8f9d03b5acb
Umar-37 <omaralhussan@outlook.com>
2021-08-04 22:02:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,26 +3,26 @@
 //var sec=1;
 //var min=1;
 
-    setInterval(setClock,1000)
+setInterval(setClock, 1000)
 
-    const hourHand=document.querySelector('[data-hour-hand')
-    const minuteHand=document.querySelector('[data-minute-hand')
-    const secondHand=document.querySelector('[data-second-hand]')
+const hourHand = document.querySelector('[data-hour-hand')
+const minuteHand = document.querySelector('[data-minute-hand')
+const secondHand = document.querySelector('[data-second-hand]')
 
-     function setClock(){
-        const thedate=new Date() ;
-        const sec=thedate.getSeconds()/60;
-        const min=(thedate.getMinutes()+sec)/60
-        const hour=(thedate.getHours()+min)/12;
+function setClock() {
+   const thedate = new Date();
+   const sec = thedate.getSeconds() / 60;
+   const min = (thedate.getMinutes() + sec) / 60
+   const hour = (thedate.getHours() + min) / 12;
 
-        setRotation(secondHand,sec)
-        setRotation(minuteHand,min)
-        setRotation(hourHand,hour)
-        }
-    
-        function setRotation(elements,rotationRatio){
-         elements.style.setProperty('--rotation',rotationRatio*360);
-         
-        }
+   setRotation(secondHand, sec)
+   setRotation(minuteHand, min)
+   setRotation(hourHand, hour)
+}
 
-       setClock()
+function setRotation(elements, rotationRatio) {
+   elements.style.setProperty('--rotation', rotationRatio * 360);
+
+}
+
+setClock()
